e22fb6cb303d691827785d031378bdcd739bcf76
Jerry Zhao <jchao@y7mail.com>
2015-04-06 17:27:31 +1000

    use handleFunc

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("hello"))
 }
