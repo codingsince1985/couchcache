0e65a8a9c1737330ae1884bded66425445a6549d
Jerry Zhao <jchao@y7mail.com>
2015-04-11 11:45:11 +1000

    url routing

--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"github.com/couchbase/go-couchbase"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var c couchbase.Client
+var p couchbase.Pool
+var b *couchbase.Bucket
+
+const bucket = "default"
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	c, _ = couchbase.Connect("http://Administrator:password@localhost:8091/")
+	p, _ = c.GetPool("default")
+	b, _ = p.GetBucketWithAuth(bucket, bucket, "")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/id/{key}", handler)
+
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(rw http.ResponseWriter, r *http.Request) {
-	bucket := "default"
-	c, _ := couchbase.Connect("http://Administrator:password@localhost:8091/")
-	p, _ := c.GetPool("default")
-	b, _ := p.GetBucketWithAuth(bucket, bucket, "")
-	v, _ := b.GetRaw("key")
+	key := mux.Vars(r)["key"]
+	v, _ := b.GetRaw(key)
 	rw.Write(v)
 }
